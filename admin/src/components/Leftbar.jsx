b36b3b97c1d97aa71ab7d8d4f61df6f355d2374f
Tafita Hery <tafitahery@gmail.com>
2023-11-19 17:36:48 +0300

    Admin Leftbar

--- admin/src/components/Leftbar.jsx
+++ admin/src/components/Leftbar.jsx
@@ -1,3 +1,35 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 250px;
+  height: calc(100vh - 51px);
+  border-right: 1px solid lightgray;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Item = styled.div`
+  padding: 10px 20px;
+  color: #555;
+  cursor: pointer;
+  &:hover {
+    background-color: #888;
+    color: #fff;
+  }
+`;
+
 export default function Leftbar() {
-  return <div>Leftbar</div>;
+  return (
+    <Container>
+      <Wrapper>
+        <Item>Machines</Item>
+        <Item>Clients</Item>
+        <Item>Pièces</Item>
+        <Item>Tech</Item>
+      </Wrapper>
+    </Container>
+  );
 }
