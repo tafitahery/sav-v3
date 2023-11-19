f0fff6ba34fd3f0855f4448601d71a5fc3ee523a
Tafita Hery <tafitahery@gmail.com>
2023-11-19 14:23:12 +0300

    Admin Login Page

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,27 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />,
+    },
+    {
+      path: '/login',
+      element: <Login />,
+    },
+    {
+      path: '/register',
+      element: <Register />,
+    },
+  ]);
+
   return (
     <div>
-      <Login />
+      <RouterProvider router={router} />
     </div>
   );
 }
