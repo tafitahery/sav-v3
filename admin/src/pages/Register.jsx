94412ec7a07adee01c12d3b8459f2bf9208819b1
Tafita Hery <tafitahery@gmail.com>
2023-11-19 14:34:29 +0300

    Admin Register Page

--- admin/src/pages/Register.jsx
+++ admin/src/pages/Register.jsx
@@ -1,3 +1,79 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  border: 1px solid lightgray;
+  padding: 30px;
+  border-radius: 10px;
+`;
+
+const Title = styled.h1`
+  text-align: center;
+  font-weight: 500;
+  color: #555;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const Input = styled.input`
+  border: none;
+  border-bottom: 1px solid lightgray;
+  padding: 10px;
+  width: 250px;
+`;
+
+const Button = styled.button`
+  border: none;
+  padding: 10px 20px;
+  font-weight: bold;
+  width: max-content;
+  color: white;
+  background-color: teal;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+const Span = styled.span`
+  font-size: 14px;
+  color: #555;
+`;
+
+const StyledLink = styled(Link)`
+  color: teal;
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
 export default function Register() {
-  return <div>Register</div>;
+  return (
+    <Container>
+      <Wrapper>
+        <Title>S'enregistrer</Title>
+        <Form>
+          <Input type="text" placeholder="Nom..." required />
+          <Input type="password" placeholder="Mot de passe..." required />
+          <Button>Enregistrer</Button>
+        </Form>
+        <Span>
+          Tu as un compte ? <StyledLink to="/login">Se connecter</StyledLink>
+        </Span>
+      </Wrapper>
+    </Container>
+  );
 }
