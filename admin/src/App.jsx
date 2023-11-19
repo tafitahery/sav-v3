81f5e0dbe61c7c2d625e9afe575341e857698de8
Tafita Hery <tafitahery@gmail.com>
2023-11-19 16:02:35 +0300

    Admin Navbar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,13 +1,57 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import {
+  Navigate,
+  Outlet,
+  RouterProvider,
+  createBrowserRouter,
+} from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Navbar from './components/Navbar';
+import Leftbar from './components/Leftbar';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+`;
 
 function App() {
+  const currentUser = true;
+
+  const Layout = () => {
+    return (
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Leftbar />
+          <Outlet />
+        </Wrapper>
+      </div>
+    );
+  };
+
+  // eslint-disable-next-line react/prop-types
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+    return children;
+  };
+
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <Home />,
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+        },
+      ],
     },
     {
       path: '/login',
--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,3 +1,55 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import Profile from '../assets/profile.png';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  height: 50px;
+  border-bottom: 1px solid lightgray;
+`;
+
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 20px;
+  color: #555;
+`;
+
+const Logo = styled(Link)`
+  font-size: 20px;
+  font-weight: 500;
+  color: inherit;
+  text-decoration: none;
+`;
+
+const User = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Avatar = styled.img`
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  object-fit: cover;
+`;
+
+const Name = styled.span``;
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <Container>
+      <Wrapper>
+        <Logo to="/">Leboadmin</Logo>
+        <User>
+          <Avatar src={Profile} alt="" />
+          <Name>John Doe</Name>
+        </User>
+      </Wrapper>
+    </Container>
+  );
 }
