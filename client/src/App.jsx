1ad2c981f6f018e9edb40c92232e85518596234c
Tafita Hery <tafitahery@gmail.com>
2023-11-30 21:47:17 +0300

    Client Login/Register Page$

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,45 @@
 import Home from './page/Home';
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from 'react-router-dom';
+import Login from './page/Login';
+import Register from './page/Register';
 
 function App() {
+  const currentUser = true;
+
+  // eslint-disable-next-line react/prop-types
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+    return children;
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: (
+        <ProtectedRoute>
+          <Home />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: '/login',
+      element: <Login />,
+    },
+    {
+      path: '/register',
+      element: <Register />,
+    },
+  ]);
+
   return (
     <div>
-      <Home />
+      <RouterProvider router={router} />
     </div>
   );
 }
--- client/src/page/Login.jsx
+++ client/src/page/Login.jsx
@@ -1,3 +1,81 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  border: 1px solid lightgray;
+  border-radius: 10px;
+  padding: 30px;
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+`;
+
+const Title = styled.h1`
+  color: #555;
+  font-size: 28px;
+  font-weight: 555;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const Input = styled.input`
+  padding: 10px;
+  width: 250px;
+  border: none;
+  border-bottom: 1px solid #888;
+`;
+
+const Button = styled.button`
+  width: max-content;
+  padding: 10px 20px;
+  border-radius: 5px;
+  border: none;
+  background-color: #888;
+  color: white;
+  cursor: pointer;
+  &:hover {
+    background-color: #555;
+  }
+`;
+
+const Span = styled.span`
+  font-size: 14px;
+  color: #888;
+`;
+
+const StyledLink = styled(Link)`
+  color: #555;
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <Container>
+      <Wrapper>
+        <Title>Connexion</Title>
+        <Form>
+          <Input type="text" placeholder="Nom" />
+          <Input type="password" placeholder="Mot de passe" />
+          <Button>Connecter</Button>
+        </Form>
+        <Span>
+          Pas de compte? <StyledLink to="/register">Enregister</StyledLink>
+        </Span>
+      </Wrapper>
+    </Container>
+  );
 }
